b3daeed9b02495cf86c5ada83166d0b21e300dcb
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-02-21 19:08:53 +0530

    home section completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import { HomeHeade } from '@/components/HomeHeade'
+import { HomeSearch } from '@/components/HomeSearch'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
       <HomeHeade />
+      <div className='flex flex-col items-center mt-24'>
+        <Image src={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"}
+        width={300} height={500} alt='google logo' priority style={{width: "auto"}}/>
+        <HomeSearch />
+      </div>
     </>
   )
 }
--- src/components/HomeHeade.jsx
+++ src/components/HomeHeade.jsx
@@ -12,7 +12,7 @@ export const HomeHeade = () => {
             <Link href={"https://image.google.com"}>
                 Image
             </Link>
-            <TbGridDots className='bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2'/>
+            <TbGridDots className='bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2 cursor-pointer'/>
             <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md 
             hover:brightness-105 hover:shadow-md transition-shadow'>Sign in</button>
         </div>
